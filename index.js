d86007bf694812e51f93b5999df206c48776bf96
Jesús Antonio Pérez Sandoval <jasp402@gmail.com>
2020-08-19 08:20:35 -0500

    feat: removed request-promise per deprecate, and remplace by node-fetch

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const request = require('request-promise');
+const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
-const YP = 'KZGH0AwL1ZmRkZmR3ZQp1AQZ';
-const YJ = 'TZGZmZmp3ZQx1ZwR1ZGpkZmR';
-const YS = 'YZwH3BGL1BGD1AQRjZwR0AmH';
-const YC = 'HAQZjZGN4AGVmAwN1AGH4Zwt';
+const YP   = 'KZGH0AwL1ZmRkZmR3ZQp1AQZ';
+const YJ   = 'TZGZmZmp3ZQx1ZwR1ZGpkZmR';
+const YS   = 'YZwH3BGL1BGD1AQRjZwR0AmH';
+const YC   = 'HAQZjZGN4AGVmAwN1AGH4Zwt';
 const SPAM = 'true';
-const V = '3.1';
+const V    = '3.1';
 
 const contains = (a, b) => {
     return a.toLowerCase().indexOf(b.toLowerCase()) >= 0;
@@ -23,59 +23,56 @@ const searchToObject = url => {
 };
 
 /* Create a random mail */
-const createMail = ()=>{
-    return request.get(`http://www.yopmail.com/es/email-generator.php`)
-        .then(result => {
-            const $ = cheerio.load(result);
-            return $('#login').val();
-
-        });
-}
+const createMail = async () => {
+    const response = await fetch(`http://www.yopmail.com/es/email-generator.php`);
+    const body     = await response.text();
+    const $        = cheerio.load(body);
+    return await $('#login').val();
+};
 
 /* Get inbox mail */
-const inbox = (id, phrase, p = 1) => {
-    return request.get(`http://m.yopmail.com/en/inbox.php?login=${id}&p=${p}&d=&ctrl=&scrl=&spam=${SPAM}&yf=005&yp=${YP}&yj=${YJ}&v=${V}&r_c=&id=`)
-        .then((result) => {
-            const $     = cheerio.load(result);
-            const mails = [];
-            let found   = false;
+const inbox = async (id, phrase, p = 1) => {
+    let mails = [];
+    let found = false;
 
-            $('.lm_m').each((index, element) => {
-                const el   = $(element);
-                const mail = ({
-                    index,
-                    when   : el.find('.lmh').text(),
-                    from   : el.find('.lmf').text(),
-                    subject: el.find('.lms_m').text(),
-                    href   : 'http://m.yopmail.com/en/' + el.attr('href'),
-                    html   : el.html()
-                });
+    const response = await fetch(`http://m.yopmail.com/en/inbox.php?login=${id}&p=${p}&d=&ctrl=&scrl=&spam=${SPAM}&yf=005&yp=${YP}&yj=${YJ}&v=${V}&r_c=&id=`);
+    const body     = await response.text();
+    const $        = cheerio.load(body);
 
-                if (phrase && !found) {
-                    found = contains(mail.from, phrase) ||
-                        contains(mail.subject, phrase);
-                }
-                mails.push(mail);
-            });
-            return {found, mails};
+    $('.lm_m').each((index, element) => {
+        const el   = $(element);
+        const mail = ({
+            index,
+            when   : el.find('.lmh').text(),
+            from   : el.find('.lmf').text(),
+            subject: el.find('.lms_m').text(),
+            href   : 'http://m.yopmail.com/en/' + el.attr('href'),
+            html   : el.html()
         });
+
+        if (phrase && !found) {
+            found = contains(mail.from, phrase) ||
+                contains(mail.subject, phrase);
+        }
+        mails.push(mail);
+    });
+    return {found, mails};
 };
 
 /* Read mail by url */
-const readMail = (url) => {
+const readMail = async (url) => {
     let params = searchToObject(url);
-    let headers = {
+    let opts = {headers: {
         'Content-Type': 'application/json',
         'Cookie'      : `compte=${params.b}; ys=${YS}; yc=${YC}`
-    };
-    return request.get(url, {'headers': headers})
-        .then(result => {
-            const $     = cheerio.load(result);
-            var t = $('#mailmillieu *').contents().map(function() {
-                return (this.type === 'text') ? $(this).text().trim()  : '';
-            }).get().filter(Boolean);
-            return t.join('\n');
-        });
+    }};
+    const response = await fetch(url, opts);
+    const body     = await response.text();
+    const $        = cheerio.load(body);
+    let t          = $('#mailmillieu *').contents().map(function () {
+        return (this.type === 'text') ? $(this).text().trim() : '';
+    }).get().filter(Boolean);
+    return t.join('\n');
 };
 
 module.exports = {
