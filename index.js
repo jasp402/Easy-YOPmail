84e0f1bed1a851b4c483e95d03ee500b1f296bf4
Jesús Antonio Pérez Sandoval <jasp402@gmail.com>
2020-08-21 17:52:45 -0500

    feat: change method

--- index.js
+++ index.js
@@ -1,103 +1,51 @@
-const fetch = require('node-fetch');
-const cheerio = require('cheerio');
-const jsPackTools = require('js-packtools')();
-const fs = require('fs');
+const YOPmail  = require('./lib');
+let express    = require('express');
+let app        = express();
 
-const YP   = 'KZGH0AwL1ZmRkZmR3ZQp1AQZ';
-const YJ   = 'TZGZmZmp3ZQx1ZwR1ZGpkZmR';
-const SPAM = 'true';
-const V    = '3.1';
+const port = process.env.PORT || 5000;
 
-const contains = (a, b) => {
-    return a.toLowerCase().indexOf(b.toLowerCase()) >= 0;
-};
-const searchToObject = url => {
-    var paramsString = url.split('?')[1];
-    var searchParams = new URLSearchParams(paramsString);
-    let array = [];
-    for (let p of searchParams) {
-        array.push(p);
+app.listen(port, function () {
+    console.log('Example app listening on port 5000!');
+});
+
+app.get('/api/getmail', function (req, res) {
+    const petition = YOPmail.createMail();
+    petition.then(response => {
+        res.send(response);
+    })
+});
+
+app.get('/api/getinbox/:mail', function (req, res) {
+    let email = undefined;
+    let query = '';
+    if((req.params.mail).indexOf('@')>-1){
+        email = req.params.mail.split('@')[0]
     }
-    const entries = new Map(array);
-    return Object.fromEntries(entries);
-};
-
-/* Create a random mail */
-const createMail = async () => {
-    try {
-        const response = await fetch(`http://www.yopmail.com/es/email-generator.php`);
-
-        //save current cookies
-        jsPackTools.validateDir('./temp');
-        const cookies = response.headers.get('set-cookie');
-        fs.writeFileSync('./temp/cookies.tmp', cookies, 'utf-8');
-
-        const body     = await response.text();
-        const $        = cheerio.load(body);
-        return await $('#login').val();
-    } catch(err) {
-        alert(err); // Failed to fetch
+    else{
+        email = req.params.mail
     }
-};
 
-/* Get inbox mail */
-const inbox = async (id, phrase, p = 1) => {
-    let mails = [];
-    let found = false;
+    if(Object.keys(req.query).length > 0){
+        let queryKey = Object.keys(req.query)[0];
+            query = req.query[queryKey];
+    }
 
-    const response = await fetch(`http://m.yopmail.com/en/inbox.php?login=${id}&p=${p}&d=&ctrl=&scrl=&spam=${SPAM}&yf=005&yp=${YP}&yj=${YJ}&v=${V}&r_c=&id=`);
-
-    //save or update current cookies
-    jsPackTools.validateDir('./temp');
-    const cookies = response.headers.get('set-cookie');
-    fs.writeFileSync('./temp/cookies.tmp', cookies, 'utf-8');
-
-    const body     = await response.text();
-    const $        = cheerio.load(body);
-
-    $('.lm_m').each((index, element) => {
-        const el   = $(element);
-        const mail = ({
-            index,
-            when   : el.find('.lmh').text(),
-            from   : el.find('.lmf').text(),
-            subject: el.find('.lms_m').text(),
-            id     : el.attr('href').split('id=')[1],
-            href   : 'http://m.yopmail.com/en/' + el.attr('href'),
-            html   : el.html()
-        });
-
-        if (phrase && !found) {
-            found = contains(mail.from, phrase) ||
-                contains(mail.subject, phrase);
-        }
-        mails.push(mail);
+    const petition = YOPmail.inbox(email, query);
+    petition.then(response => {
+        res.end(JSON.stringify(response, null, ' '));
     });
-    return {found, mails};
-};
+});
 
-/* Read mail by url */
-const readMail = async (url) => {
-    let params = searchToObject(url);
-    let rawCookies = fs.readFileSync('./temp/cookies.tmp', "utf-8");
-    let setCookies = rawCookies.split(',').map(x => x.split(';')).flat();
-    let cookies = setCookies.filter(x => x.indexOf('ys') > -1 || x.indexOf('yc') > -1).join(';');
-
-    let opts = {headers: {
-        'Content-Type': 'application/json',
-        'Cookie'      : `compte=${params.b}; ${cookies}`
-    }};
-    const response = await fetch(url, opts);
-    const body     = await response.text();
-    const $        = cheerio.load(body);
-    let t          = $('#mailmillieu *').contents().map(function () {
-        return (this.type === 'text') ? $(this).text().trim() : '';
-    }).get().filter(Boolean);
-    return t.join('\n');
-};
-
-module.exports = {
-    inbox     : inbox,
-    readMail  : readMail,
-    createMail: createMail
-};
+app.get('/api/readmail/:mail/:id', function (req, res) {
+    let url = 'http://m.yopmail.com/en/m.php?';
+    if(req.params.mail && req.params.id){
+        url += `b=${req.params.mail}&id=${req.params.id}`
+    }else{
+        res.end('error! require mail and id');
+    }
+    console.log(url);
+    const petition = YOPmail.readMail(url);
+    petition.then(response => {
+        res.end(response);
+    })
+});
